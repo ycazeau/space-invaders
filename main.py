f2d0faca9634b7234e42f6ff1b473449d39dfe0b
ycazeau <ycazeau23@gmail.com>
2020-07-27 16:21:55 -0400

    Adding Player Image

--- main.py
+++ main.py
@@ -8,17 +8,28 @@ HEIGHT = 600
 # Create the screnn
 screen = pygame.display.set_mode((WIDTH , HEIGHT))
 
-#Title and Icon
+#Caption and Icon
 pygame.display.set_caption("Space Invaders by Yves Ronaldo CAZEAU")
-icon = pygame.image.load('images/enemy.png')
+icon = pygame.image.load('images/ufo.png')
 pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('images/player.png')
+playerX = 370
+playerY = 500
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
 
 # Game loop
 running = True
 while running:
+    # Background
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
