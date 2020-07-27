463a85caae9d8d8b6b170730e236288401c71fb3
ycazeau <ycazeau23@gmail.com>
2020-07-27 16:01:25 -0400

    Adding Title and Icon

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import pygame
 
 # Initialize the pygame
 pygame.init()
+WIDTH = 800
+HEIGHT = 600
 
 # Create the screnn
-screen = pygame.display.set_mode((600 , 400))
+screen = pygame.display.set_mode((WIDTH , HEIGHT))
+
+#Title and Icon
+pygame.display.set_caption("Space Invaders by Yves Ronaldo CAZEAU")
+icon = pygame.image.load('images/enemy.png')
+pygame.display.set_icon(icon)
 
 # Game loop
 running = True
@@ -12,3 +19,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.update()
